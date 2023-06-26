9a8d35a5e3bbbc34beb5a0895b3b0d3df6c6d2d8
name <ksyusha0kubay@gmail.com>
2023-06-26 11:56:14 +0300

    Add manifest

---
+++ custom_addons/demo/manifest.py
@@ -0,0 +1,15 @@
+{
+    "name": "Demo Module",
+    "version": "1.0",
+    "category": "Uncategorized",
+    "summary": "Just simple test module Odoo 16",
+    "author": "Oksana",
+    "depends": ["base"],
+    "data": [
+        "demo/views/demo_demo_views.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
